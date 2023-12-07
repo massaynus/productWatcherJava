e59172d57e90eed87ac2180739111c2182c8cab3
Yassine Elmassaoudi <yassine.elmassaoudi@imedia24.de>
2023-12-07 16:39:06 +0100

    setup java project

---
+++ src/main/java/com/imedia24/productWatcher/ProductWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.imedia24.productWatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imedia24/productWatcher/ProductWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imedia24.productWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
